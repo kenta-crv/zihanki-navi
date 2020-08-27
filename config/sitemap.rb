bcfdb967ce6b228ebd94becdcb75997d5e909302
kenta-crv <adp@crold-valieat.jp>
2020-08-27 13:20:58 +0900

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,18 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://eatery-japan.com"
+SitemapGenerator::Sitemap.default_host = "https://shiokuritai.com"
 
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
 
 add "/" , changefreq: 'daily', priority: 1.0
+add "guide" , changefreq: 'daily', priority: 0.8 #エンジランス
+add "faq" , changefreq: 'daily', priority: 0.8  #ムービーズ
+add "co" , changefreq: 'daily', priority: 0.8  #メールマーケティング
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+add "/contact" , changefreq: 'daily', priority: 0.3
+add "/lps" , changefreq: 'daily', priority: 0.3
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
@@ -19,13 +26,18 @@ add "/" , changefreq: 'daily', priority: 1.0
   #
   # Examples:
   #
-   Add '/stores'
+  add posts_path, :priority => 1.0, :changefreq => 'daily'
+  Post.find_each do |post|
+    add post_path(post), :lastmod => post.updated_at
+  end
 
-     add stores_path, :priority => 0.7, :changefreq => 'daily'
+  add lists_path, :priority => 1.0, :changefreq => 'daily'
+  List.find_each do |list|
+    add list_path(list), :lastmod => list.updated_at
+  end
 
-   Add all stores:
-
-     store.find_each do |store|
-       add store_path(store), :lastmod => store.updated_at
-     end
+  add companies_path, :priority => 1.0, :changefreq => 'daily'
+  Company.find_each do |company|
+    add company_path(company), :lastmod => company.updated_at
+  end
 end
