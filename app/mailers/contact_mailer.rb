91dc9f3979746b5624d55e512e21e589c528dabb
kenta-crv <okuyamakenta@gmail.com>
2021-07-29 15:43:31 +0900

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def received_email(contact)
     @contact = contact
     mail to: "info@comicomi.cc"
-    mail(subject: '人材紹介一括見積り『Comicomi』よりお問い合わせがありました') do |format|
+    mail(subject: '『Comicomi』よりお問い合わせがありました') do |format|
       format.text
     end
   end
@@ -11,7 +11,7 @@ class ContactMailer < ActionMailer::Base
   def send_email(contact)
     @contact = contact
     mail to: contact.email
-    mail(subject: '人材紹介一括見積り『Comicomi』にお問い合わせ頂きありがとうございます') do |format|
+    mail(subject: '『Comicomi』にお問い合わせ頂きありがとうございます') do |format|
       format.text
     end
   end
